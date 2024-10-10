d5f112f81ae93885a4d5262aaf1c74e7ba997de2
aqnaazmy <aqnaazmy@gmail.com>
2024-10-10 22:45:51 +0700

    initial project

---
+++ src/week1/DataType.kt
@@ -0,0 +1,26 @@
+package week1
+
+fun main() {
+
+    //Tipe Data string
+    val nama : String = "aqna"
+//    nama = "plugin"
+
+    //Tipe Data Int
+    val umur : Int = 15
+
+    val result = "nama saya $nama umur saya $umur"
+
+    println(result)
+
+    // Tipe Data Double
+    val double = 12.5
+
+    //Tipe Data Char
+    val char = 'A'
+
+    //Tipe Data Boolean
+    val benar = true
+    val salah = false
+//    println(nama)
+}
